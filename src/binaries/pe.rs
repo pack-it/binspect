0caee304f734cc4c7efaf2dd3a23d10e35494c89
Frank <mail@franku.nl>
2026-06-28 23:07:50 +0200

    Remove pe-loader rpath support

--- src/binaries/pe.rs
+++ src/binaries/pe.rs
@@ -1,5 +1,4 @@
 use lief::{generic::Section, pe::Binary};
-use pe_loader_lib::rpath_section::RPathSection;
 
 use crate::{Command, Result};
 
@@ -25,33 +24,6 @@ pub fn inspect_pe(_command: Command, binary: Binary) -> Result<()> {
         println!("\t{} (delay)", import.name());
 
         found = true;
-    }
-    if !found {
-        println!(" None");
-    }
-
-    print!("RPath:");
-    let mut found = false;
-    for section in binary.sections() {
-        if section.name() != ".rpath" {
-            continue;
-        }
-        let rpath: RPathSection = match section.try_into() {
-            Ok(rpath) => rpath,
-            Err(_) => {
-                println!("WARNING: Found invalid .rpath section.");
-                continue;
-            },
-        };
-
-        for entry in rpath.entries {
-            if !found {
-                println!();
-            }
-
-            println!("\t{}", entry.path);
-            found = true;
-        }
     }
     if !found {
         println!(" None");
